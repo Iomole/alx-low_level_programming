e87ebf94c22d5e28f98eb6a2adab1535ef1a2e1a
Iomole <omolec1@gmail.com>
2023-03-16 16:20:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main-this is the main function of a C program
+ *that prints exactly and that piece of art is useful
+ *Dora Korpar, followed by a new l*ine, to the standard error.
+ *Return: return one on success
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
