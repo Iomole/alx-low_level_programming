2ccf28dd51a7c4c817a556689e801a5e1c71e05d
Iomole <omolec1@gmail.com>
2023-03-16 13:00:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- This is the main function
+ * Return: This function will return o
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
