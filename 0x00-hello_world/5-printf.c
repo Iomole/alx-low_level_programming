1843ec9dd91bf47867fef86c474b9fb2a385eba2
Iomole <omolec1@gmail.com>
2023-03-16 13:08:52 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-This ths main function
+ * Return: this returns zero when task is complete
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
