0dfd0e932a2153f17d3f0ba65e09577b6edee268
Iomole <omolec1@gmail.com>
2023-03-20 11:19:17 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: zero
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+
+		putchar(l);
+		putchar('\n');
+	}
+		return (0);
+}
