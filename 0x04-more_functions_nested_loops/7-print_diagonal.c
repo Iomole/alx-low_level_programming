b51aaa4a07fab747c9ed96d42039e710164e7666
Iomole <omolec1@gmail.com>
2023-03-24 22:03:52 +0000

    7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,28 +6,24 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int m;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (m = 0; m < n; m++)
-		{
-			if (n <= 0)
-				_putchar('\n');
-			else
-			{
-				if (m == i)
-				{
-					_putchar('\\');
-				}
-				else if (m < i)
-					_putchar(' ');
-			}
-
-		}
 		_putchar('\n');
 
-	}
+	} else
+	{
+		int i, j;
 
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
