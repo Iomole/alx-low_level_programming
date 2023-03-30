ae50afb22ef7f7d063a81e2147d176861a498854
Iomole <omolec1@gmail.com>
2023-03-30 19:58:14 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,19 +8,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int m = 0;
+	int m = 0;
 
-        while (m < n && src[m] != '\0')
-        {
-                dest[m] = src[m];
-                m++;
-        }
+	while (m < n && src[m] != '\0')
+	{
+		dest[m] = src[m];
+		m++;
+	}
 
-        while (m < n)
-        {
-                dest[m] = '\0';
-                m++;
-        }
-        return (dest);
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+	return (dest);
 }
 
