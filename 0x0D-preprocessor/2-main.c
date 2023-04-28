5880d595e3ba6119e59202ebcf393b16fac2e269
Iomole <omolec1@gmail.com>
2023-04-28 09:13:28 -0700

    commit 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the file
+ *
+ * Return: Zero if successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
