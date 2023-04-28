958e74b62979be9059497d09b9b5f622cef40774
Iomole <omolec1@gmail.com>
2023-04-28 11:19:52 -0700

    commit dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,14 +17,14 @@ struct dog
 };
 
 /**
- * dog_type - typedefinition  for struct dog
+ * dog_t - typedefinition  for struct dog
  */
-typedef struct dog dog_type;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_type *new_dog(char *name, float age, char *owner);
-void free_dog(dog_type *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
