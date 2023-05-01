f39c4eb173929f79173c280f79cd93f7104ae513
Iomole <omolec1@gmail.com>
2023-05-01 01:00:37 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A function that
+ * prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: zero successful
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _conv_str_to_int(char *s);
+
+#endif
+
